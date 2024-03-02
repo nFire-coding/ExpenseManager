b3abcf31727221dab0fc8da148a9fb683f3c659f
zGiuly <giuliapvpgirls@gmail.com>
2024-03-02 16:12:52 +0100

    Aggiunto il file escluso

---
+++ Gestore di Entrate/Gestore di Entrate/Modules/Mesi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestore_di_Entrate.Modules
+{
+    enum Mesi
+    {
+        Gen,
+        Feb,
+        Mar,
+        Apr,
+        Mag,
+        Giu,
+        Lug,
+        Ago,
+        Set,
+        Ott,
+        Nov,
+        Dic
+    }
+}
